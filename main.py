e758ce4a3de6ee92bf5e84902034f9d14d69d56f
parinita2312 <mahawar.0223@gmail.com>
2026-03-13 16:34:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from utils.preprocessing import load_corpus
 from models.language_model import LanguageModel
 from models.noisy_channel import SpellChecker
