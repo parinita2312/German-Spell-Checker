9648e2ac3320fdc1867074ab1589d3eb42ab8418
parinita2312 <mahawar.0223@gmail.com>
2026-03-13 16:17:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 import os
+
 sys.path.append(os.path.dirname(__file__))
+
 from preprocessing import load_corpus
 from language_model import LanguageModel
 from noisy_channel import SpellChecker
+
 tokens = load_corpus("corpus.txt")
+
 lm = LanguageModel(tokens)
 checker = SpellChecker(lm)
+
 while True:
     word = input("Enter word: ")
     print("Correction:", checker.correct(word))
