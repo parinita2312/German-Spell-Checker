f92d64e703ed59d02089b94d1fc4c23e502d2dba
parinita2312 <mahawar.0223@gmail.com>
2026-03-13 16:13:37 +0530

    Refactor imports and update corpus file path

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 from preprocessing import load_corpus
-from models.language_model import LanguageModel
-from models.noisy_channel import SpellChecker
-tokens = load_corpus("data/corpus.txt")
+from language_model import LanguageModel
+from noisy_channel import SpellChecker
+tokens = load_corpus("corpus.txt")
 lm = LanguageModel(tokens)
 checker = SpellChecker(lm)
 while True:
     word = input("Enter word: ")
-    print("Correction:",checker.correct(word))
+    print("Correction:", checker.correct(word))
