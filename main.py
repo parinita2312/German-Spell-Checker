6b1b04096f495dc23b290c28f95936dd6cecfc38
parinita2312 <mahawar.0223@gmail.com>
2026-03-13 16:02:14 +0530

    Implement spell checker with language model

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils.preprocessing import load_corpus
+from models.language_model import LanguageModel
+from models.noisy_channel import SpellChecker
+tokens = load_corpus("data/corpus.txt")
+lm = LanguageModel(tokens)
+checker = SpellChecker(lm)
+while True:
+    word = input("Enter word: ")
+    print("Correction:",checker.correct(word))
