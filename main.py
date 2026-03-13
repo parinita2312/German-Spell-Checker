aefae6b089df431b85b7e1a39acdc7dd0b2ffcb7
parinita2312 <mahawar.0223@gmail.com>
2026-03-13 16:28:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 
 sys.path.append(os.getcwd())
 
-from preprocessing import load_corpus
-from language_model import LanguageModel
-from noisy_channel import SpellChecker
+from utils.preprocessing import load_corpus
+from models.language_model import LanguageModel
+from models.noisy_channel import SpellChecker
 
-tokens = load_corpus("corpus.txt")
+tokens = load_corpus("data/corpus.txt")
 
 lm = LanguageModel(tokens)
 checker = SpellChecker(lm)
